5ad4a9a3a2371ca7aab75c04dd2839c7cb94df48
zainab <zainab.hammami.zh@gmail.com>
2017-10-13 19:52:25 +0300

    fix files

--- app/CurrentArticle.jsx
+++
@@ -1,8 +0,0 @@
-// var CurrentArticle = (props) => (
-// 	<div className="current">
-// 	<div className="reading" src={props.article.category[0].url}></div>
-// 	</div>
-// 	)
-// CurrentArticle.propTypes = {
-//   article: React.PropTypes.array.isRequired
-// };
